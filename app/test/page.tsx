e5ca8ad05f6b435529d9f84b67205fc6e26b17b4
Parth arora <Partharora3068@gmail.com>
2025-11-18 00:27:04 +0530

    refactor: remove unused imports from TestPage component

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,18 +1,13 @@
 "use client"
 
-import CursorTrail from "@/components/cursor-trail";
-import {LoaderCircle} from "@/components/loader-circle";
-import { RevealButton, RevealButtonIcon } from "@/components/reveal-button";
-import { RollOverText } from "@/components/roll-over-text";
-import {  SaveToggle} from "@/components/save-toggle";
-import { Bookmark, CheckIcon, SaveIcon, SplinePointerIcon } from "lucide-react";
+
 
 
 export default function TestPage() {
   return(
     <div className=" flex items-center justify-center h-screen">
 
-      <CursorTrail/>
+
     </div>
 );
 }
