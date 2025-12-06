03a20fe0ad4390c6ceae4e4b11185eb4202ee58f
Parth Arora <partharora3068@gmail.com>
2025-12-06 20:10:55 +0530

    fix bugs

--- components/demo/carousal-01-demo.tsx
+++ components/demo/carousal-01-demo.tsx
@@ -6,7 +6,6 @@ export function Carousal01Demo() {
   const images = [
     { src: '/images/usefork7.jpeg' },
     { src: '/images/usefork6.jpeg' },
-    { src: '/images/usefork2.jpeg' },
     { src: '/images/usefork5.jpeg' },
     { src: '/images/usefork4.jpeg' },
   ];
