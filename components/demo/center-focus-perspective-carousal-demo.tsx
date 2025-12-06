98c500b823c5dca2e950075a9eaad404a668b2d8
Parth Arora <partharora3068@gmail.com>
2025-12-06 13:07:54 +0530

    image remove from swiper

--- components/demo/center-focus-perspective-carousal-demo.tsx
+++ components/demo/center-focus-perspective-carousal-demo.tsx
@@ -5,7 +5,6 @@ import { CenterFocusSwiper } from "../carousals/card-carousal";
 
 export function TestimonialSwiperDemo() {
 const images = [
-  { src: "/images/usefork2.jpeg" },
   { src: "/images/usefork6.jpeg" },
   { src: "/images/usefork7.jpeg" },
   { src: "/images/usefork5.jpeg" },
